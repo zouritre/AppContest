f2965c359d11406d35458ea4a71a77f0f44d706a
Zouritre <timouf97400@live.Fr>
2023-01-27 15:43:50 +0100

    Initial Commit

---
+++ AppContest/AppContestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppContestApp.swift
+//  AppContest
+//
+//  Created by Bertrand Dalleau on 27/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct AppContestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
